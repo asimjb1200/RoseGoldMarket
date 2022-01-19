f5744bc78de2e928117db4864356133d5f151f56
Asim <asimjbrown@gmail.com>
2022-01-19 15:31:57 -0600

    Initial Commit

---
+++ RoseGoldMarket/RoseGoldMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoseGoldMarketApp.swift
+//  RoseGoldMarket
+//
+//  Created by Asim Brown on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RoseGoldMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
