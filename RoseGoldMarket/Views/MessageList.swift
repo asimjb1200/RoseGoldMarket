287296141a70db432e2ee3e68ef052ff40d4f9d5
Asim <asimjbrown@gmail.com>
2023-04-26 19:07:00 -0500

    updating the view of the message list

--- RoseGoldMarket/Views/MessageList.swift
+++ RoseGoldMarket/Views/MessageList.swift
@@ -24,53 +24,51 @@ struct MessageList: View {
             VStack {
                     if !viewModel.latestMessages.isEmpty {
                         List {
-                                ForEach(viewModel.latestMessages, id: \.id) { chatPreview in
-                                    NavigationLink(destination: MessageThread(receiverId: chatPreview.recid == currentUser.accountId ? chatPreview.senderid : chatPreview.recid, receiverUsername: chatPreview.nonViewingUsersUsername)){
-                                        HStack(spacing: 10) {
-                                            AsyncImage(url: URL(string: "https://rosegoldgardens.com/api/images/avatars/\(chatPreview.nonViewingUsersUsername).jpg")) { phase in
-                                                if let image = phase.image {
-                                                    image
-                                                        .resizable()
-                                                        .scaledToFill()
-                                                        .clipShape(Circle())
-                                                        .frame(width: 60, height: 60)
-                                                } else if phase.error != nil {
-                                                    Color.red
-                                                } else {
-                                                    ProgressView()
-                                                        .foregroundColor(Color("MainColor"))
-                                                        .frame(width: 25, height: 25)
-                                                }
+                            ForEach(viewModel.latestMessages, id: \.id) { chatPreview in
+                                NavigationLink(destination: MessageThread(receiverId: chatPreview.recid == currentUser.accountId ? chatPreview.senderid : chatPreview.recid, receiverUsername: chatPreview.nonViewingUsersUsername)){
+                                    HStack(spacing: 10) {
+                                        AsyncImage(url: URL(string: "https://rosegoldgardens.com/api/images/avatars/\(chatPreview.nonViewingUsersUsername).jpg")) { phase in
+                                            if let image = phase.image {
+                                                image
+                                                    .resizable()
+                                                    .scaledToFill()
+                                                    .clipShape(Circle())
+                                                    .frame(width: 60, height: 60)
+                                            } else if phase.error != nil {
+                                                Color.red
+                                            } else {
+                                                ProgressView()
+                                                    .foregroundColor(Color("MainColor"))
+                                                    .frame(width: 25, height: 25)
                                             }
-                                            
-                                            VStack(alignment: .leading) {
-                                                if isUnreadMessage(chatPreview: chatPreview) {
-                                                    Text(chatPreview.nonViewingUsersUsername)
-                                                        .badge(getUnreadCountForChat(chatPreview: chatPreview))
-                                                        .fontWeight(.bold)
-                                                        .font(.headline)
-                                                } else {
-                                                    Text(chatPreview.nonViewingUsersUsername)
-                                                }
-                                                HStack {
-                                                    Text(chatPreview.message)
-                                                    Spacer()
-                                                    Text(chatPreview.timestamp.formatted(date: .numeric, time: .omitted))
-                                                        .font(.caption2)
-                                                }
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                
-                                            }.foregroundColor(Color("MainColor"))
-                                            Spacer()
-                                            
                                         }
-                                        .padding([.leading, .trailing])
-                                        .frame(maxHeight: 70)
                                         
-                                    }.isDetailLink(false)
-                                    //Divider()
-                                }
-                        }.listStyle(PlainListStyle()).navigationTitle("Conversations")
+                                        VStack(alignment: .leading) {
+                                            if isUnreadMessage(chatPreview: chatPreview) {
+                                                Text(chatPreview.nonViewingUsersUsername)
+                                                    .badge(getUnreadCountForChat(chatPreview: chatPreview))
+                                                    .fontWeight(.bold)
+                                                    .font(.headline)
+                                            } else {
+                                                Text(chatPreview.nonViewingUsersUsername)
+                                            }
+                                            HStack {
+                                                Text(chatPreview.message)
+                                                Spacer()
+                                                Text(chatPreview.timestamp.formatted(date: .numeric, time: .omitted))
+                                                    .font(.caption2)
+                                            }
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            
+                                        }.foregroundColor(Color("MainColor"))
+                                        Spacer()
+                                    }
+                                    .padding([.leading, .trailing])
+                                    .frame(maxHeight: 50)
+                                    
+                                }.isDetailLink(false)
+                            }
+                        }.listStyle(PlainListStyle())
                     } else {
                         Text("No Conversations Yet").font(.largeTitle).frame(maxWidth: .infinity).fontWeight(.bold)
                     }
